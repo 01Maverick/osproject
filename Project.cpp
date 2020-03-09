b0b0b6265b93e416e8fec2359d992c5d2960d5b5
01Maverick <61726301+01Maverick@users.noreply.github.com>
2020-03-09 14:10:16 +0530

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -15,3 +15,25 @@ struct Process_Data
 	int Res_time;
 
 };
+struct Process_Data current;
+typedef struct Process_Data P_d ;
+
+bool idsort(const P_d& x , const P_d& y)
+{
+	return x.Pid < y.Pid;
+}
+bool arrivalsort( const P_d& x ,const P_d& y)
+{
+	if(x.A_time < y.A_time)
+		return true;
+	else if(x.A_time > y.A_time)
+		return false;
+	if(x.Priority < y.Priority)
+		return true;
+	else if(x.Priority > y.Priority)
+		return false;
+	if(x.Pid < y.Pid)
+		return true;
+
+	return false;
+}
