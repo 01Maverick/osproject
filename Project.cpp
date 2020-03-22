08b9b5d59dd4f3c81228bbd6a8949d7db4ee9e3f
01Maverick <61726301+01Maverick@users.noreply.github.com>
2020-03-22 15:48:54 +0530

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -59,4 +59,50 @@ struct comPare
 	}
 	
 };
+/**To check the Input **/
+void my_check(vector<P_d> mv)
+{
+	for(unsigned int i= 0; i < mv.size() ;i++)
+	{
+		cout<<" Pid :"<<mv[i].Pid<<" _time : "<<mv[i].A_time<<" B_time : "<<mv[i].B_time<<" Priority : "<<mv[i].Priority<<endl;
+	}
+
+}
+
+
+int main()
+{
+	int i;
+	vector< P_d > input;
+	vector<P_d> input_copy;
+	P_d temp;
+	int pq_process = 0; // for PQ process
+	int rq_process = 0; // for RQ process
+	int A_time;
+	int B_time;
+	int Pid;
+	int Priority;
+	int n;
+	int clock;
+	int total_exection_time = 0;
+	cin>>n;
+	for( i= 0; i< n; i++ )
+	{
+		cin>>Pid>>A_time>>B_time>>Priority;
+		temp.Num = i+1;
+		temp.A_time = A_time;
+		temp.B_time = B_time;
+		temp.R_time = B_time;
+		temp.Pid = Pid;
+		temp.Priority = Priority;
+		input.push_back(temp);
+	}
+	input_copy = input;
+	sort( input.begin(), input.end(), arrivalsort );
+    	//cout<<"arrivalsort : "<<endl;
+    	//my_check( input ); // To check the sort unomment it
+    	total_exection_time = total_exection_time + input[0].A_time;
+
+    
+
 
