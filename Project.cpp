2e8100cbe4c7513f5ec402c928c0e0f894ab04a9
01Maverick <61726301+01Maverick@users.noreply.github.com>
2020-03-26 10:01:10 +0530

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -17,4 +17,74 @@ struct Process_Data
 };
 struct Process_Data current;
 typedef struct Process_Data P_d ;
+bool idsort(const P_d& x , const P_d& y)
+{
+	return x.Pid < y.Pid;
+}
+bool arrivalsort( const P_d& x ,const P_d& y)
+{
+	if(x.A_time < y.A_time)
+		return true;
+	else if(x.A_time > y.A_time)
+		return false;
+	if(x.Priority < y.Priority)
+		return true;
+	else if(x.Priority > y.Priority)
+		return false;
+	if(x.Pid < y.Pid)
+		return true;
+
+	return false;
+}
+
+bool Numsort( const P_d& x ,const P_d& y)
+{
+	return x.Num < y.Num;
+}
+/*Sorting on the base of Priority if that same then on the base of PID*/
+struct comPare
+{
+	bool operator()(const P_d& x ,const P_d& y)
+	{
+		if( x.Priority > y.Priority )
+			return true;
+		else if( x.Priority < y.Priority )
+			return false;
+		if( x.Pid > y.Pid )
+			return true;
+
+		return false;
+		
+	}
+	
+};
+/**To check the Input **/
+void my_check(vector<P_d> mv)
+{
+	for(unsigned int i= 0; i < mv.size() ;i++)
+	{
+		cout<<" Pid :"<<mv[i].Pid<<" _time : "<<mv[i].A_time<<" B_time : "<<mv[i].B_time<<" Priority : "<<mv[i].Priority<<endl;
+	}
+
+}
+
+
+int main()
+{
+	int i;
+	vector< P_d > input;
+	vector<P_d> input_copy;
+	P_d temp;
+	int pq_process = 0; // for PQ process
+	int rq_process = 0; // for RQ process
+	int A_time;
+	int B_time;
+	int Pid;
+	int Priority;
+	int n;
+	int clock;
+	int total_exection_time = 0;
+	cin>>n;
+	
+
 
